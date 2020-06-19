77cc5a556db6ad48dbfb88842ddd007d232a1302
Harbdollar <34744075+Harbdollar@users.noreply.github.com>
2020-06-19 10:11:17 +0100

    Android locally resolved

--- lib/src/screen/reservationsTabs/UpcomingTab.dart
+++ lib/src/screen/reservationsTabs/UpcomingTab.dart
@@ -9,7 +9,6 @@ import 'package:hostapp/src/style/AppFontSizes.dart';
 import 'package:hostapp/src/style/AppTextStyle.dart';
 import 'package:hostapp/src/widget/ReservationWidget.dart';
 import 'package:provider_architecture/provider_architecture.dart';
-
 import 'package:string_validator/string_validator.dart';
 import 'package:hostapp/src/model/getReservationMode.dart';
 import 'package:hostapp/src/widget/ui_helpers.dart';
